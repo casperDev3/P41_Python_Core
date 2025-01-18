6b2c64f2c0214b34d6703955451697d737489787
Ihor Lialiuk <ihor.lialiuk@gmail.com>
2025-01-18 16:55:01 +0200

    add primitive menu optioms

---
+++ lessons/loops.py
@@ -0,0 +1,60 @@
+from colorama import init, Fore
+
+# for loops
+# for i in range(1, 100, 1):
+#     print(i)
+
+# array_fruits = ["apple", "banana", "cherry"]
+
+# counter = 0
+# for item in array_fruits:
+#     counter += 1
+#     if item == "banana":
+#         print("I found banana!")
+#         break
+#     continue
+
+
+# start_number = 1
+#
+# while start_number < 100:
+#     print(start_number)
+#     start_number += 1
+#     if start_number == 50:
+#         break
+
+# Виводимо вітальне повідомлення
+print(Fore.GREEN + "Програма запущена!\n")
+while True:
+    # Виводимо меню опцій
+    print(Fore.BLUE + "Виберіть опцію:")
+    print(Fore.BLUE + "0. Почати СПАМ")
+    print(Fore.BLUE + "1. Вивести всі продукти")
+    print(Fore.BLUE + "2. Вивести продукти дешевше 100 грн")
+    print(Fore.BLUE + "3. Вивести продукти з категорії електроніка")
+    print(Fore.BLUE + "4. Вивести продукти в алфавітному порядку")
+    print(Fore.RED + "5. Вийти з програми")
+
+    # Отримуємо вибір користувача
+    user_input = int(input(Fore.YELLOW + "\nВведіть опцію: "))
+
+    # Перевіряємо вибір користувача
+    if user_input == 0:
+        for i in range(1, 10000, 1):
+            print(f"SPAM----{i}")
+            continue
+        continue
+    elif user_input == 1:
+        print("Опція 1")
+    elif user_input == 2:
+        print("Опція 2")
+    elif user_input == 3:
+        print("Опція 3")
+    elif user_input == 4:
+        print("Опція 4")
+    elif user_input == 5:
+        print("До побачення!")
+        break
+    else:
+        print("Виберіть опцію від 1 до 5")
+        continue
