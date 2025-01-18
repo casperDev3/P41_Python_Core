9ab2c160989a52cbcecdc1a4af2e3167c56b769d
Ihor Lialiuk <ihor.lialiuk@gmail.com>
2025-01-18 17:24:27 +0200

    add all lesson

--- class_w/quiz.py
+++ class_w/quiz.py
@@ -1,3 +1,5 @@
+from colorama import Fore
+
 questions = {
     "Зумери": [
         {"question": "Що означає 'yeet'?",
@@ -9,7 +11,8 @@ questions = {
     ],
     "Бумери": [
         {"question": "Що означає 'дискета'?",
-         "options": ["USB-накопичувач", "Зовнішній жорсткий диск", "Знімний носій даних у вигляді квадратної пластикової коробки"],
+         "options": ["USB-накопичувач", "Зовнішній жорсткий диск",
+                     "Знімний носій даних у вигляді квадратної пластикової коробки"],
          "answer": "Знімний носій даних у вигляді квадратної пластикової коробки"},
         {"question": "Як бумери можуть назвати 'інтернет'?",
          "options": ["Всесвітня павутина", "Глобальна система", "Телеграф"],
@@ -24,3 +27,47 @@ questions = {
          "answer": "Паперові сторінки"}
     ]
 }
+
+# START GAME
+print(Fore.GREEN + "Вітаю вас у грі \"Вгадай слово з епохи\"!")
+
+while True:
+    # Виводимо список епох
+    print(Fore.YELLOW + "Виберіть епоху:"
+                        "\n1. Вікторина")
+    print(Fore.RED + "0. Вихід\n")
+
+    # Вибір епохи
+    action = int(input(Fore.BLUE + "Ваш вибір: "))
+
+    # Перевірка на вибір
+    if action == 1:
+        open_main_menu = False
+        for universe, quiz in questions.items():
+            print(f"\n{Fore.GREEN}Тема: {universe}{Fore.RESET}")
+            for question_data in quiz:
+                print(question_data["question"])
+                for i, option in enumerate(question_data["options"], 1):
+                    print(f"{i}. {option}")
+
+                # Запит відповіді від користувача
+                print(Fore.RED + "\n0. Вийти в голоне меню")
+                answer = input("Введіть номер правильної відповіді: ")
+                if answer == "0":
+                    open_main_menu = True
+                    break
+
+                # Перевірка відповіді
+                if question_data["options"][int(answer) - 1] == question_data["answer"]:
+                    print(f"{Fore.GREEN}Правильно!{Fore.RESET}\n")
+                else:
+                    print(f"{Fore.RED}Неправильно! Правильна відповідь була: {question_data['answer']}{Fore.RESET}\n")
+            if open_main_menu:
+                break
+
+    elif action == 0:
+        print(Fore.RED + "Дякую за гру!")
+        break
+    else:
+        print(Fore.RED + "Виберіть доступну опцію!")
+        continue
