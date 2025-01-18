393a4dbb5cec0fc95b4f168891fc100bb688308c
Ihor Lialiuk <ihor.lialiuk@gmail.com>
2025-01-18 16:06:24 +0200

    add first lassons

---
+++ lessons/conditions.py
@@ -0,0 +1,48 @@
+
+# entered_age = int(input("Enter your age: "))
+# print(entered_age < 18)
+
+# if 0 <= entered_age < 18:
+#     print("Іди спи дитино")
+# elif 18 <= entered_age < 21:
+#     print("Ти вже дорослий в Україні")
+# elif 21 <= entered_age < 65:
+#     print("Ти вже можеш пити в США")
+# elif 100 <= entered_age < 150:
+#     print("Ти вже столітній")
+# elif 65 <= entered_age < 100:
+#     print("Ти пенсіонер")
+# elif 150 <= entered_age < 200:
+#     print("Ти вже мертвий")
+# elif 200 <= entered_age < 300:
+#     print("Ти вже мумія")
+# elif 300 <= entered_age < 500:
+#     print("Ти вже вампір")
+# elif 500 <= entered_age < 1000:
+#     print("Ти вже динозавр")
+# elif entered_age < 0:
+#     print("Ти ще не народився")
+# else:
+#     print("Ти вже не відомий в історії людства індивід")
+
+# age = 15
+
+# print(age < 18) # Logical expression simple
+# print(age > 18) # Logical expression simple
+
+# print(age == 18) # Logical expression simple
+# print(age != 18) # Logical expression simple
+
+# AND - &&
+# print(age >= 18 and age <= 21) # False
+# print(age < 18 and age > 100) # False
+# print(age > 21 and age < -1) # False
+# print(age > 10 and age < 20) # True
+
+# OR - ||
+# print(age >= 18 or age <= 21) # True
+# print(age < 18 or age > 100) # True
+# print(age > 21 or age < -1) # False
+# print(age > 10 or age < 20) # True
+
+
